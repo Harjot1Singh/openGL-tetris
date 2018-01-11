08c647d66a1681a7262f6035452975281e892556
Harjot Singh <harjot@harkul.com>
2018-01-11 15:14:15 +0000

    add tetromino layouts

--- Tetromino.cpp
+++ Tetromino.cpp
@@ -1,8 +1,55 @@
+#include <vector>
+#include <map>
+
 #include "Tetromino.h"
 
-/* Create a 4x1 grid for I */
-I::I() : Grid(4, 1)
+using namespace std;
+
+/* Holds the dimensions of each Tetromino block */
+const map<TetrominoType, vector<vector<int>>> dimensions = {
+  { O, {
+      { 1, 1 },
+      { 1, 1 }
+    } },
+  { I, { { 1, 1, 1, 1 } } },
+  { T, {
+      { 1, 1, 1 },
+      { 0, 1, 0 }
+    } },
+  { S, {
+      { 0, 1, 1 },
+      { 1, 1, 0 }
+  } },
+  { Z, {
+      { 1, 1, 0 },
+      { 0, 1, 1 }
+  } },
+  { J, {
+      { 1, 1, 1 },
+      { 0, 0, 1 }
+  } },
+  { L, {
+      { 1, 1, 1 },
+      { 1, 0, 0}
+  } }
+},
+
+};
+
+/* Get the tetromino width from the dimensions map */
+getTetrominoWidth(TetrominoType type)
 {
-  // Set each block in I to not empty
-  
+  return dimensions.at(type).size();
+}
+
+/* Get the tetromino height from the dimensions map */
+getTetrominoHeight(TetrominoType type)
+{
+  return dimensions.at(type).at(0).size();
+}
+
+Tetromino::Tetromino(TetrominoType type) :
+  Grid(getTetrominoWidth(type), getTetrominoHeight(type))
+{
+
 }
--- Tetromino.h
+++ Tetromino.h
@@ -3,14 +3,11 @@
 #include "IDisplay.h"
 #include "Grid.h"
 
-/* Base class for Tetrominos */
-class Tetromino : public Grid, public IDisplay {};
+enum TetrominoType { I, J, O, L, S, Z, T };
 
-/* Class per Tetromino */
-class I : public Tetromino {	I(); };
-class O : public Tetromino {	O(); };
-class T : public Tetromino {	T(); };
-class L : public Tetromino {	L(); };
-class J : public Tetromino {	J(); };
-class Z : public Tetromino {	Z(); };
-class S : public Tetromino {	S(); };
+/* Constructs the provided Tetromino */
+class Tetromino : public Grid, public IDisplay
+{
+	public:
+		Tetromino(TetrominoType type);
+};
